b448c17ec47ff77a0df868befa34baaf9f9b428a
Anangbril <89025118+Anangbril@users.noreply.github.com>
2022-01-13 21:50:22 +0700

    Null

--- js/hello.js
+++ js/hello.js
@@ -1,15 +1,10 @@
-//undefined is a dataType
-// let name;
-// if (name == undefined) {
-//     alert("undefined");
-// } else {
-//     alert("defined");
-// }
+//tipe data null
+let fullName = null;
 
-//undefined data array value
-const name = ["anang", " Richo", 1];
-if (name[2] == undefined) {
-    alert("data tidak ditemukan");
+if (fullName === undefined) {
+    alert("Undefined");
+} else if (fullName === null) {
+    alert("null");
 } else {
-    alert(`data ditemukan ${name[2]}`);
+    alert("not both of them")
 }
