9162298d256c41425308286591f296acff7fdcaf
Anangbril <89025118+Anangbril@users.noreply.github.com>
2022-01-13 16:58:38 +0700

    percabangan if
    include else if dan else

--- js/hello.js
+++ js/hello.js
@@ -1,27 +1,9 @@
-// tipe data object
-const manusia = {};
-
-//mengubah property diobject
-manusia["nama"] = "Rico Kurniawan utomo";
-manusia["gender"] = "Laki-laki";
-manusia["alamat"] = "jln nangka gang jambu kel semangka";
-manusia["umur"] = 30;
-
-console.table(manusia);
-delete manusia["umur"];
-console.table(manusia);
-
-// membuat object dengan properties
-const hewan = {
-    nama: "burung", //nama tidak pakai petik dua karena hanya satu kata
-    "jenis makanan": "pemakan tumbuh-tumbuhan",//jenis makanan memakai petik dua karena lebih dari satu kata
-    "usia hidup": "10 tahun",
-    "jarak tempuh terbang satuan KM": 300
-};
-console.table(hewan);
-document.writeln(hewan); //tidak muncul datanya
-
-//mengakses property object
-console.info(`Nama : ${hewan.nama}`);
-console.info(`usia hidup : ${hewan["usia hidup"]}`);
-
+//if expression
+const value = 90;
+if (value < 10) {
+    document.writeln("kondisi benar");
+} else if (value < 100) {
+    document.writeln("<h1>Ini percabangan if else</h1>")
+} else {
+    document.writeln("<h1>Ini percabangan else</h1>")
+}
