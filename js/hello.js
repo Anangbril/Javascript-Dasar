4672c9ab34923c2ed6f2121c207024ed11007010
Anangbril <89025118+Anangbril@users.noreply.github.com>
2022-01-14 09:54:42 +0700

    Falsy And Truthy

--- js/hello.js
+++ js/hello.js
@@ -1,21 +1,14 @@
-//OPTIONAL CHAINING
+//  FALSY AND TRUTHY
 
-// let object = {};
-// alert(`isi object : ${object.person.nama}`);
+let data = "";
+if (data) {
+    document.writeln("TRUE");
+} else {
+    document.writeln("FALSE");
+}
 
-let person = {
-    address: {
-        country: "Indonesia Raya"
-    }
-};
-// let country;
-// if (person.address !== undefined && person.address !== null) { //pengecekan menggunakan if
-//     country = person.address.country;
-// }
-alert(person?.address?.country); //using Optional chaining
-// document.writeln(`${country}`);
-
-/**catatan
- * jika person tidak nullish maka akan di akses addressnya, tanda tanya untuk mengecek atau memastikan apakah person nullish atau tidak
- * jika address tidak nullish lagi maka akan di akses countrynya
+/**
+ * FALSY  adalah value yang ketika dalam kontext boolean, dia dianggap false
+ * truthy adalah kebalikan atau negasi dari data falsy.
+ * DATA FALSY :: (false, 0,-0, "",'',``, null, undefined, NaN)
  */
