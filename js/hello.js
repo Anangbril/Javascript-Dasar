b2c34e2e5d51401c0ff60b132e6bbbe423649d82
Anangbril <89025118+Anangbril@users.noreply.github.com>
2022-01-14 10:31:32 +0700

    Operator OR di non Boolean

--- js/hello.js
+++ js/hello.js
@@ -1,14 +1,23 @@
-//  FALSY AND TRUTHY
+// OPERASI OR (||) 
+/**
+ * operasi or akan mengambil nilai yang paling kiri jika keduanya berniali benar
+ * sebaliknya nilai paling kanan akan diambil jika keduanya bernilai falsy
+ * nilai yang di ambil dan dikeluarkan adalah nilai truthy
+ */
+console.info("hello" || ""); //hello
+console.info("" || {}); //{}
+console.info("0" || "NOL"); //0
+console.info(0 || "NOL"); //NOL
+console.info(null || "NULL"); //NULL
+console.info(undefined || "UNDEFINED"); //UNDEFINED
+console.info(0 || false); //false
 
-let data = "";
-if (data) {
-    document.writeln("TRUE");
-} else {
-    document.writeln("FALSE");
+
+
+const person = {
+    firstName: "",
+    lastName: "Brilyansyah"
 }
 
-/**
- * FALSY  adalah value yang ketika dalam kontext boolean, dia dianggap false
- * truthy adalah kebalikan atau negasi dari data falsy.
- * DATA FALSY :: (false, 0,-0, "",'',``, null, undefined, NaN)
- */
+const data = person.firstName || person.lastName;
+console.info(data); //Brilyansyah
